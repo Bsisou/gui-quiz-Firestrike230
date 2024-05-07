8e9ec3e92f0db53dd68b02af6803b0137e7678a9
Taufeeq Ameen <161650149+Firestrike230@users.noreply.github.com>
2024-05-07 23:39:54 +0000

    Adding dictionary of questions and answers

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from tkinter import *
+import random
+questions_answers = {
+    1: ["What is 2 + 2?", ["Four (4)", "Five (5)", "Three (3)", "Six (6)", "Eight (8)"], "Four (4)"],
+    2: ["What is 5 x 3?", ["Eight (8)", "Fifteen (15)", "Ten (10)", "Twenty (20)", "Eighteen (18)"], "Fifteen (15)"],
+    3: ["What is 10 / 2?", ["Four (4     0)", "Two (2)", "Six (6)", "Five (5)", "Three (3)"], "Five (5)"],
+    4: ["What is 8 - 4?", ["Five (5)", "Two (2)", "Three (3)", "Six (6)", "Four (4)"], "Four (4)"],
+    5: ["What is 3 squared?", ["Six (6)", "Nine (9)", "Seven (7)", " Twelve (12)", "Five (5)"], "Nine (9)"],
+    7: ["What is 12 x 10?", ["One-hundred and twenty (120)", "One-hundred (100)", "One-hundred and ten (110)", "One-hundred and thirty (130)", "One-hundred and five (105)"], "One-hundred and twenty (120)"],
+    8: ["What is 20 / 4?", ["Four (4)", "Three (3)", "Five (5)", "Six (6)", "Two (2)"], "Five (5)"],
+    9: ["What is 7 + 3?", ["Ten (10)", "Eight (8)", "Six (6)", "Twelve (12)", "Five (5)"], "Ten (10)"],
+    10: ["What is 15 - 6?", ["Ten (10)", "Eight (8)", "Nine (9)", "Seven (7)", "Five (5)"], "Nine (9)"]
+}
